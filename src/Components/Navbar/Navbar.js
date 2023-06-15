76d0c5bfb12a097d04072aa774a893916b5da237
avijit <avijitroy2944@gmail.com>
2023-06-15 10:36:20 +0530

    initial

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="mainNavbar">
+      <div>
+        <p>Social</p>
+      </div>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <div>
+          <img src="" alt="" />
+          <p>Avijit</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
