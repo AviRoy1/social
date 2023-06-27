1dea2fa3e02346e90df481c9953d482b9c71bb4e
avijit <avijitroy2944@gmail.com>
2023-06-27 11:43:25 +0530

    add

--- src/Components/Redux/apiCall.js
+++ src/Components/Redux/apiCall.js
@@ -10,3 +10,29 @@ export const login = async (dispatch, user) => {
     dispatch(loginFailure());
   }
 };
+
+export const VerifyEmail = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post(
+      `http://localhost:5000/api/user/verify/email`,
+      user
+    );
+    dispatch(loginSuccess(res.data));
+  } catch (error) {
+    dispatch(loginFailure());
+  }
+};
+
+export const signup = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post(
+      `http://localhost:5000/api/user/create/user`,
+      user
+    );
+    dispatch(loginSuccess(res.data));
+  } catch (error) {
+    dispatch(loginFailure());
+  }
+};
--- src/Pages/Register/Signup.js
+++ src/Pages/Register/Signup.js
@@ -1,16 +1,71 @@
 import React, { useState } from "react";
 import "./signup.css";
-import { UseSelector, useDispatch } from "react-redux";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { signup } from "../../Components/Redux/apiCall";
+import app from "../../firebase";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const Signup = () => {
-  const dispatch = useDispatch();
-
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [phoneNumber, setPhonenumber] = useState("");
+  const [file, setFile] = useState(null);
 
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  console.log(user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const fileName = new Date().getTime() + file?.name;
+    const storage = getStorage(app);
+    const StorageRef = ref(storage, fileName);
+
+    const uploadTask = uploadBytesResumable(StorageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          signup(dispatch, {
+            email,
+            password,
+            phoneNumber,
+            username,
+            profile: downloadURL,
+          });
+
+          window.location.reload(true);
+        });
+      }
+    );
+  };
   return (
     <div className="mainContainerforSignup">
       <div className="submainContainer">
@@ -24,6 +79,13 @@ const Signup = () => {
         </div>
         <div style={{ flex: 3 }}>
           <p className="createaccountText">Create New Account</p>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            className="inputText"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
           <input
             type="text"
             placeholder="UserName"
@@ -48,7 +110,9 @@ const Signup = () => {
             onChange={(e) => setPassword(e.target.value)}
             className="inputText"
           />
-          <button className="btnForSignup">Signup</button>
+          <button className="btnForSignup" onClick={handleClick}>
+            Signup
+          </button>
           <Link to="/">
             <p style={{ marginLeft: "30%", textAlign: "start" }}>
               Already have an account
